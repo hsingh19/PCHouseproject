1e21aa9e7c45a9e7393e7add4781175705f73b8c
suniljadhav709 <suniljadhav709@gmail.com>
2017-08-08 15:59:07 -0400

    login and registeration Merged with basic body, Footer and header

---
+++ src/app/dump/dummyData.ts
@@ -0,0 +1,16 @@
+import {User} from './user';
+
+export const Users : User[] = [
+    {
+       id : 1,
+       name : 'sunil',
+       password : 'password',
+       email : 'sunil@gmail.com'
+    },
+    {
+       id : 2,
+       name : 'harshit',
+       password : 'password',
+       email : 'harshit@gmail.com'
+    }
+] 
